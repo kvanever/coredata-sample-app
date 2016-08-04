9a5638778b62eff36357882a2ce6176a97cf71a7
Kevin VanEvery <kvanever@gmail.com>
2016-08-04 12:52:10 -0700

    add persistent store coordinator to manage the URLs in the managed object model and create a SQLite db

--- CoreDataList/CoreDataStack.swift
+++ CoreDataList/CoreDataStack.swift
@@ -11,15 +11,36 @@ import CoreData
 
 class DataController: NSObject {
     
-    lazy var applicationDocumentsDirectory: NSURL = {
+    private lazy var applicationDocumentsDirectory: NSURL = {
         
        let urls = NSFileManager.defaultManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask)
         return urls[urls.endIndex.predecessor()]
     }()
     
-    lazy var managedObjectModel: NSManagedObjectModel = {
+    private lazy var managedObjectModel: NSManagedObjectModel = {
        let modelURL = NSBundle.mainBundle().URLForResource("ToDoList", withExtension: "momd")!
         return NSManagedObjectModel(contentsOfURL: modelURL)!
+    }()
+    
+    private lazy var persistentStoreCoordinater: NSPersistentStoreCoordinator = {
+       let coordinator = NSPersistentStoreCoordinator(managedObjectModel: self.managedObjectModel)
+        let url = self.applicationDocumentsDirectory.URLByAppendingPathComponent("ToDoList.sqlite")
+        
+        do {
+            try coordinator.addPersistentStoreWithType(NSSQLiteStoreType, configuration: nil, URL: url, options: nil)
+        } catch {
+            let userInfo: [String: AnyObject] = [
+                NSLocalizedDescriptionKey: "Failed to initialize the application's saved data",
+                NSLocalizedFailureReasonErrorKey: "There was an error creating or loading the application's saved data",
+                NSUnderlyingErrorKey: error as NSError
+            ]
+            
+            let wrappedError = NSError(domain: "com.kevinvanevery.CoreDataError", code: 9999, userInfo: userInfo)
+            print("Unresolved error \(wrappedError), \(wrappedError.userInfo)")
+            abort()
+        }
+        
+        return coordinator
     }()
 
 }
