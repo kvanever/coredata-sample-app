0a5eae22d4076d74770b0b68cef20eef15449ace
Kevin VanEvery <kvanever@gmail.com>
2016-08-04 12:21:52 -0700

    Initial Commit

---
+++ CoreDataList/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoreDataList
+//
+//  Created by Kevin VanEvery on 8/4/16.
+//  Copyright © 2016 Kevin VanEvery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
