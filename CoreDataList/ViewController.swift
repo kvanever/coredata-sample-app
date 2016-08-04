d5de3aee80a63076941ca511e4e99eabf4993423
Kevin VanEvery <kvanever@gmail.com>
2016-08-04 13:56:29 -0700

    add navigation controllers to main storyboard

--- CoreDataList/ViewController.swift
+++ CoreDataList/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func cancel(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
 
+    @IBAction func save(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+
+    }
 }
 
